539e00e2e4d665f8f74c5d546d1c9f06f0e1e033
Omar Cherif <omarcherif2866@gmail.com>
2026-03-27 15:47:37 +0100

    add feedbacks ans actu

--- xicom-angular/src/app/service/auth.service.ts
+++ xicom-angular/src/app/service/auth.service.ts
@@ -146,8 +146,8 @@ import { EncryptionService } from './encryption-service.service';
 })
 export class AuthService {
 
-  // private apiUrl = "/api/auth";
-  private apiUrl = "http://localhost:9090/auth";
+  private apiUrl = "/api/auth";
+  // private apiUrl = "http://localhost:9090/auth";
 
   private apiUrlUser = "/api/users";
   private localStorageKey = "userAuth";
